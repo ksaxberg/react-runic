08abadcddd0119ada84f33a1c9f6fa36f3e19a55
Kyle Saxberg <15057490+ksaxberg@users.noreply.github.com>
2018-10-27 01:16:16 -0700

    remove example button

--- lib/components/Runic.jsx
+++ lib/components/Runic.jsx
@@ -2,27 +2,22 @@
 import * as React from 'react';
 import '../styles/Runic.css';
 
-import getSvg, { queryForSvgs } from '../svgs/helper.js';
+import { queryForSvgs } from '../svgs/helper.js';
 
 
 type Props = {
   text: string,
 }
 
-/* eslint-disable */
 export default class Runic extends React.PureComponent<Props> {
   static defaultProps = {
-    text: 'Add Text'
+    text: 'Add Text',
+    style: {},
   };
 
   render() {
     return (
-      <div>
-        From: '{this.props.text}' to: 
-        <div className="svg-holder">{queryForSvgs(this.props.text)}</div>
-      </div>
+      <div style={this.props.style} className="svg-holder">{queryForSvgs(this.props.text)}</div>
     );
   }
 }
-
-/* eslint-enable */
--- stories/index.js
+++ stories/index.js
@@ -2,24 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withNotes } from '@storybook/addon-notes';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { withKnobs, text, object } from '@storybook/addon-knobs/react';
 
 import { backgrounds} from './backgrounds';
-import Button from '../lib/elements/Button';
 import Runic from '../lib/components/Runic';
-
-storiesOf('Button', module)
-  .addDecorator(withKnobs)
-  .addDecorator(backgrounds)
-  .add('with text', withNotes('Example note on basic button')(() => (
-    <Button onClick={action('clicked')}>{text('Child: ', 'Hello Button')}</Button>
-  )))
-  .add('with some emoji', withNotes('Example note on emoji button')(() => (
-    <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  )));
 
 storiesOf('Runic', module)
   .addDecorator(withKnobs)
   .addDecorator(backgrounds)
   .add('no text provided', () => (<Runic />))
-  .add('text provided', () => <Runic text={text('Text: ', 'Hey there')} />);
+  .add('text provided', () => <Runic style={object('Style', {})} text={text('Text: ', 'Hey there')} />);

d76fba7c3709e37a14c82fd89b79c5f750a99b38
ksaxberg <kylesaxberg@gmail.com>
2018-10-27 15:01:22 -0700

    no need for build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _Runic = require('./components/Runic');
-
-var _Runic2 = _interopRequireDefault(_Runic);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = { Runic: _Runic2.default };
